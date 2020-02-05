10dea0caa76a1617821241195b9bc0a21514e5b8
Janne Leppänen <janne@janneleppanen.com>
2020-02-05 20:03:17 +0200

    feat(fetch): Fetch RSS feeds on main view

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # The RSS
 
 This a hobby project to test Tailwindcss and netlify functions.
+
+## Development
+
+Run `npm start` and browse `http://localhost:3000`
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const INITIAL_FEEDS = [
+  "https://feed.syntax.fm/rss",
+  "https://rss.simplecast.com/podcasts/279/rss"
+];
+
+interface Feed {
+  title: string;
+  items: FeedItem[];
+}
+
+interface FeedItem {
+  title: string;
+  link: string;
+  content: string;
+}
+
+type FeedList = Feed[];
 
 const App = () => {
+  const [feeds, setFeeds] = React.useState<FeedList>([]);
+
+  React.useEffect(() => {
+    const data = JSON.stringify({
+      rssFeeds: INITIAL_FEEDS
+    });
+
+    fetch("http://localhost:3000/.netlify/functions/parse-rss-feeds", {
+      method: "POST",
+      body: data
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        const allFeeds = [];
+        for (let feed in response) {
+          allFeeds.push(response[feed]);
+        }
+        setFeeds(allFeeds);
+      });
+
+    return () => {};
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {feeds.map(feed => (
+        <div>
+          <h2>{feed.title}</h2>
+          {feed.items.map(feedItem => (
+            <div>
+              <a href={feedItem.link}>{feedItem.title}</a>
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
