588155db7c9ebb43c0e7d64e28e2a57eb405b8e6
Janne Leppänen <janne@janneleppanen.com>
2020-02-08 14:57:24 +0200

    feat(router): Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Sidebar from "./components/Sidebar";
+import Home from "./components/Home";
+import Feed from "./components/Feed";
 
 export interface Feed {
   title: string;
@@ -18,66 +21,34 @@ export interface FeedItem {
 
 export type FeedList = Feed[];
 
-const FEED_PARSE_URL = process.env.REACT_APP_FEED_PARSE_URL;
-
 const App = () => {
   const [feeds, setFeeds] = React.useState<FeedList>(
     JSON.parse(localStorage.getItem("feeds") || "") || []
   );
-  const [newFeedURL, setNewFeedURL] = React.useState<string>("");
 
   React.useEffect(() => {
     localStorage.setItem("feeds", JSON.stringify(feeds));
     return () => {};
   }, [feeds]);
 
-  const handleFormSubmit = async () => {
-    await loadFeed(newFeedURL);
-    setNewFeedURL("");
-  };
-
-  const loadFeed = async (url: string) => {
-    const res = await fetch(FEED_PARSE_URL || "", {
-      method: "POST",
-      body: JSON.stringify({ rssFeeds: [url] })
-    });
-    const data = await res.json();
-    const newFeed = data[url];
-    setFeeds([...feeds, newFeed]);
-  };
-
   return (
-    <div className="h-screen flex">
-      <Sidebar feeds={feeds} />
-      <main className="flex-1">
-        <div className="max-w-screen-lg mx-auto my-10 p-10">
-          <form
-            onSubmit={e => {
-              e.preventDefault();
-              handleFormSubmit();
-            }}
-          >
-            <input
-              className="border border-gray-300 border-solid rounded-md p-2 mr-2"
-              type="text"
-              value={newFeedURL}
-              onChange={e => setNewFeedURL(e.currentTarget.value)}
-            />
-            <button className="py-2 px-5 bg-green-600 text-white rounded-md">
-              Add new feed
-            </button>
-          </form>
-
-          {feeds.map(feed => (
-            <div key={feed.title}>
-              <h2 className="text-2xl font-bold my-6">
-                {feed.title} <small>({feed.items.length})</small>
-              </h2>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <Router>
+      <div className="h-screen flex">
+        <Sidebar feeds={feeds} />
+        <main className="flex-1">
+          <div className="max-w-screen-lg mx-auto my-10 p-10">
+            <Switch>
+              <Route path="/feed/:feedId">
+                <Feed />
+              </Route>
+              <Route path="/">
+                <Home feeds={feeds} setFeeds={setFeeds} />
+              </Route>
+            </Switch>
+          </div>
+        </main>
+      </div>
+    </Router>
   );
 };
 
