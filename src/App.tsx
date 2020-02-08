16e0d30485597ed463562d78a0d21dbf5dd0a7f1
Janne Leppänen <janne@janneleppanen.com>
2020-02-08 20:53:10 +0200

    feat(feed): Show feed items on feed page

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Feed from "./components/Feed";
 
 export interface Feed {
   title: string;
+  description: string;
   image?: {
     url: string;
   };
@@ -16,8 +17,11 @@ export interface Feed {
 
 export interface FeedItem {
   title: string;
-  link: string;
+  contentSnippet: string;
   content: string;
+  link: string;
+  pubDate: string;
+  isoDate: string;
 }
 
 export type FeedList = Feed[];
@@ -26,14 +30,12 @@ const App = () => {
   const { feeds, addFeed } = useFeeds();
 
   return (
-    <div className="h-screen flex">
+    <div className="min-h-screen flex items-stretch">
       <Sidebar feeds={feeds} />
       <main className="flex-1">
-        <div className="max-w-screen-lg mx-auto my-10 p-10">
+        <div className="max-w-screen-md mx-auto my-10 p-10">
           <Switch>
-            <Route path="/feed/:feedId">
-              <Feed />
-            </Route>
+            <Route path="/feed/:feedId" component={Feed}></Route>
             <Route path="/">
               <Home feeds={feeds} addFeed={addFeed} />
             </Route>
