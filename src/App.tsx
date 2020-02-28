41b48b284e21aa1b4561d433a1a29a2a04ba686d
Janne Leppänen <janne@janneleppanen.com>
2020-02-28 20:14:45 +0200

    feat(sidebar): Add a button for syncing all feeds at once

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,14 @@ interface Props {
   loadFeed: (url: string) => void;
 }
 
-const App = ({ feeds }: Props) => {
+const App = ({ feeds, loadFeed }: Props) => {
+  const syncAllFeeds = () => {
+    feeds.forEach(feed => loadFeed(feed.url));
+  };
+
   return (
     <div className="flex h-screen text-sm font-sans">
-      <Sidebar feeds={feeds} />
+      <Sidebar feeds={feeds} onSyncClick={syncAllFeeds} />
       <main className="flex flex-1 flex-col overflow-auto h-full">
         <div className="max-w-screen-md w-full mx-auto my-10 p-10">
           <Switch>
