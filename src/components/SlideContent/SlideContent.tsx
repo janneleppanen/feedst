76bf4b0992fa3e7247be29a97a6735de72dbe499
Janne Leppänen <janne@janneleppanen.com>
2020-03-05 20:01:45 +0200

    feat(feeditem): Add a transition for opening and closing side content

--- src/components/SlideContent/SlideContent.tsx
+++ src/components/SlideContent/SlideContent.tsx
@@ -1,30 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 import usePortal from "../../utils/usePortal";
 
 interface Props {
   onClose: () => void;
+  isOpen: boolean;
 }
 
-const SlideContent: React.FC<Props> = ({ children, onClose }) => {
+const SlideContent: React.FC<Props> = ({ children, onClose, isOpen }) => {
   const target = usePortal("slide-content-root");
   const overlayElement = React.useRef(null);
 
   return ReactDOM.createPortal(
-    <div
-      ref={overlayElement}
-      className="fixed dark-overlay top-0 bottom-0 left-0 right-0 flex items-center justify-center"
-      onMouseDown={e => {
-        if (e.target === overlayElement.current) {
-          onClose();
-        }
-      }}
+    <ReactCSSTransitionGroup
+      transitionName="slide"
+      transitionEnterTimeout={250}
+      transitionLeaveTimeout={150}
     >
-      <div className="fixed right-0 top-0 bottom-0 bg-white p-10 w-full max-w-6xl shadow-2xl overflow-auto">
-        {children}
-      </div>
-    </div>,
+      {isOpen && (
+        <div
+          ref={overlayElement}
+          className="fixed dark-overlay top-0 bottom-0 left-0 right-0 flex items-center justify-center"
+          onMouseDown={e => {
+            if (e.target === overlayElement.current) {
+              onClose();
+            }
+          }}
+        >
+          <div className="fixed right-0 top-0 bottom-0 bg-white p-10 w-full max-w-6xl shadow-2xl overflow-auto">
+            {children}
+          </div>
+        </div>
+      )}
+    </ReactCSSTransitionGroup>,
     target
   );
 };
